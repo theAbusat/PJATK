9f9278346db8999a7e1669768afe77c0cac5f67b
Abusat <82516569+theAbusat@users.noreply.github.com>
2022-11-30 22:52:05 +0100

    Delete BYT_56 directory

--- BYT_56/Calculator/src/Chain.java
+++
@@ -1,4 +0,0 @@
-public interface Chain {
-    void nextChain(Chain nextChain);
-    void calculate(Calculation calculation);
-}
--- BYT_56/Memento/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Originator originator = new Originator();
-        originator.setState("start");
-        originator.setState("update1");
-        originator.setState("update2");
-        originator.undo();
-    }
-}
